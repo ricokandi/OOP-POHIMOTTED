a04b5b803b042f3fbc96ef99a45b340543f25ecf
Rico Kandi <rico.kandi@voco.ee>
2025-01-08 13:50:50 +0200

    oi oi oi

---
+++ oop_simple.py
@@ -0,0 +1,10 @@
+"""Simple class."""
+
+
+class Student:
+    """konstruktori loomine."""
+
+    def __init__(self, name):
+        """konstruktor."""
+        self.name = name
+        self.finished = False
